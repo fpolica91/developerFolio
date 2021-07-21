bd5f7366185d5cdd0b0962beebaa35039a1ae7ca
fabricio policarpo <fabriciopolicarpo0@gmail.com>
2021-07-21 13:04:30 -0400

    adding projects

--- src/portfolio.js
+++ src/portfolio.js
@@ -199,21 +199,21 @@ const bigProjects = {
   subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
   projects: [
     {
-      image: require("./assets/images/saayaHealthLogo.webp"),
-      projectName: "Saayahealth",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/logo.png"),
+      projectName: "Schoolahoop",
+      projectDesc: "Project that helps parents find schools for their children",
       footerLink: [
         {
           name: "Visit Website",
-          url: "http://saayahealth.com/"
+          url: "http://schoolahoop.org/"
         }
         //  you can add extra buttons here.
       ]
     },
     {
-      image: require("./assets/images/nextuLogo.webp"),
-      projectName: "Nextu",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/nickel.png"),
+      projectName: "Project Nickel",
+      projectDesc: "Project that helps users find the price that each school spends per pupil",
       footerLink: [
         {
           name: "Visit Website",
